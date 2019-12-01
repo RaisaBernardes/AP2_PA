3eb6039eb68f97ddcb29d804b5d2144d13cf32e5
raisabernardes <raisa.rmro@gmail.com>
2019-12-01 03:56:50 -0300

    - 12th commit - No ActionBar, Fields of NewContactActivity done.

--- app/src/main/java/com/faculdade/pa_ap2/Activities/Mp3Activity.java
+++ app/src/main/java/com/faculdade/pa_ap2/Activities/Mp3Activity.java
@@ -4,13 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.faculdade.libdownloadmanager.DownloadFacade;
 import com.faculdade.pa_ap2.R;
 
 public class Mp3Activity extends AppCompatActivity {
+
+    private String vamoLa = "Downloadando...";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mp3_activity);
+
+        DownloadFacade.getInstance().getDownloadController().download(this, vamoLa);
+
+
     }
 }
